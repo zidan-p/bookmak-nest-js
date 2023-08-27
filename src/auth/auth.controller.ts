2b2002290c6a8bbd0aed746f4c69d86f0a7f67e7
zidan-p <zidanputrarahman153@gmail.com>
2023-08-27 21:39:02 +0700

    add route and handler

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,33 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 
 
 
 
-
-@Controller()
+// where the request routed to each service or logic.
+// prefix path to request, it will pass to request method as prefix (see below).
+@Controller("auth")
 export class AuthController{
 
   // the injection will be handled by nestjs
   constructor(
     private authService: AuthService
   ){}
+
+  // decarator to define what method to handle and what route.
+  @Post("signin") // :POST /auth/signin
+  signin(){
+    // nestjs automatically set that datatype in header coresponding with the return type
+    // so the datatype below will be plain text
+    // return "I am signin"; 
+
+    // it will be object json
+    return {msg: "hello i'm signin"}
+  }
+
+  @Post("signout") // :POST /auth/signout
+  signout(){
+    return "I am signout";
+  }
+
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@nestjs/common";
 
 
+// where the logic took place
 @Injectable({})
 export class AuthService{}
