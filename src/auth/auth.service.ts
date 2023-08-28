7633b4020834e2e2f4bd836916180cfd9fd5f737
zidan-p <zidanputrarahman153@gmail.com>
2023-08-28 14:13:39 +0700

    handle prisma error

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDto } from "./dto";
 import * as argon from "argon2";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
 // where the logic took place
 @Injectable({})
@@ -13,36 +14,46 @@ export class AuthService{
   
   async signup(dto: AuthDto){
 
-    // generate the password hash
-    const hash = await argon.hash(dto.password);
+    try {
+      // generate the password hash
+      const hash = await argon.hash(dto.password);
+  
+      // save user in database
+      const newUser = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash: hash,
+        },
+      })
+  
+      delete newUser.hash; // don't let hash exposed
+  
+      // return new user
+      return newUser;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError){
+        // constraint failed, ex: multiple same value for uniqe field
+        if(error.code === "P2002")
+          throw new ForbiddenException('Credentials taken');
+      }
 
-    // save user in database
-    const newUser = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        hash: hash,
-      },
+      throw error;
+    }
 
-      // only return true value
-      // select: {
-      //   id        : true,
-      //   email     : true,
-      //   createdAt : true
-      // }
-    })
-
-    delete newUser.hash; // don't let hash exposed
-
-    // return new user
-    return newUser;
   }
 
   signin(){
-    // nestjs automatically set that datatype in header coresponding with the return type
-    // so the datatype below will be plain text
-    // return "I am signin"; 
 
-    // it will be object json
+    // find the user by email
+
+    //throw exception when empty
+
+
+    // compare password
+
+    // throw when failed / don't match
+
+    // send back user
     return {msg: "i'm signin to get bookmark"}
   }
 
