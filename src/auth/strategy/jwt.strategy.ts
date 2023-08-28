45312b0b968cc742a1882c54bb25b94afe823c96
zidan-p <zidanputrarahman153@gmail.com>
2023-08-28 23:25:40 +0700

    implementatio in user

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -2,17 +2,39 @@ import { Injectable } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { PassportStrategy } from "@nestjs/passport";
 import { ExtractJwt, Strategy } from "passport-jwt";
+import { PrismaService } from "src/prisma/prisma.service";
 
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy){
+export class JwtStrategy extends PassportStrategy(Strategy, "jwt"){
   
   constructor(
-    private config: ConfigService
+    config: ConfigService,
+    private prisma: PrismaService
   ){
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: config.get("JWT_SECRET")
     });
+  }
+
+  async validate(payload: {sub: number, email: string}){
+
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id: payload.sub
+      }
+    })
+
+    delete user.hash;
+
+    // if user is null it will throw credenstial exception to user
+
+    // it will be appended in request object.
+    // like in teh express there are middlewares. so when we try to chain from one middleware 
+    // to another middleware, we use `next(req)` function with req object to make data still consistent.
+    // in here (validate), we will pass payload as new properties in req express object 
+    // by return-ed it.
+    return user;
   }
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
+import { JwtGuard } from 'src/auth/guard';
 
-@Controller('user')
-export class UserController {}
+@Controller('users')
+export class UserController {
+
+  @UseGuards(JwtGuard)
+  @Get("me")
+  getMe(@Req() req: Request){
+    return req.user
+  }
+}
