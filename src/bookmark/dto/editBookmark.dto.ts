d83ff600e237d94acdb076cce21cfb4b137fe422
zidan-p <zidanputrarahman153@gmail.com>
2023-08-31 13:11:29 +0700

    empty structure controller and service bookmark

---
+++ src/bookmark/dto/editBookmark.dto.ts
@@ -0,0 +1,17 @@
+import { IsNotEmpty, IsOptional, IsString } from "class-validator";
+
+export class EditBookmarkDto{
+
+  @IsString()
+  @IsOptional()
+  title?      : string;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+  
+  
+  @IsString()
+  @IsOptional()
+  link?       : string;
+}
