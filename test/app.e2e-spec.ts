a5693c79011a510c69fdbd344089dc2983093da9
zidan-p <zidanputrarahman153@gmail.com>
2023-08-31 11:03:13 +0700

    add more in e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,7 +1,11 @@
 import {Test} from '@nestjs/testing'
+import * as pactum from "pactum"; 
+
 import { AppModule } from '../src/app.module'
 import { INestApplication, ValidationPipe } from '@nestjs/common'
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from 'src/auth/dto';
+
 
 
 
@@ -22,10 +26,14 @@ describe("App e2e", () => {
     )
 
     await app.init();
+    await app.listen(3006);
 
     // make prisma clean database every test
     prisma = app.get(PrismaService);
     await prisma.cleanDb();
+
+    // set url for pactum http request
+    pactum.request.setBaseUrl("http://localhost:3006");
   })
 
   afterAll(() => {
@@ -33,13 +41,110 @@ describe("App e2e", () => {
   })
 
   describe('Auth', () => {
-    describe("Sign up", () => {});
+    describe("Sign up", () => {
+      const dto: AuthDto = {
+        email: "zidan@email.com",
+        password: "saya ada 2"
+      }
+      it("Should throw if email empty", () => {
+        return pactum
+          .spec()
+          .post("/auth/signup")
+          .withBody({
+            password: dto.password,
+            email: "" 
+          })
+          .expectStatus(400)
+      })
+      it("Should throw if password empty", () => {
+        return pactum
+          .spec()
+          .post("/auth/signup")
+          .withBody({
+            password: "",
+            email: dto.email
+          })
+          .expectStatus(400)
+      })
+      it("Should throw if body empty", () => {
+        return pactum
+          .spec()
+          .post("/auth/signup")
+          .withBody({
+            password: "",
+            email: ""
+          })
+          .expectStatus(400)
+      })
+      it("Should signup", () => {
+        return pactum
+          .spec()
+          .post("/auth/signup")
+          .withBody(dto)
+          .expectStatus(201)
+      })
+    });
 
-    describe("Sign in", () => {});
+    describe("Sign in", () => {
+      const dto: AuthDto = {
+        email: "zidan@email.com",
+        password: "saya ada 2"
+      }
+      it("Should throw if email empty", () => {
+        return pactum
+          .spec()
+          .post("/auth/signin")
+          .withBody({
+            password: dto.password,
+            email: "" 
+          })
+          .expectStatus(400)
+      })
+      it("Should throw if password empty", () => {
+        return pactum
+          .spec()
+          .post("/auth/signin")
+          .withBody({
+            password: "",
+            email: dto.email
+          })
+          .expectStatus(400)
+      })
+      it("Should throw if body empty", () => {
+        return pactum
+          .spec()
+          .post("/auth/signin")
+          .withBody({
+            password: "",
+            email: ""
+          })
+          .expectStatus(400)
+      })
+      it("Should signin", () => {
+        return pactum
+          .spec()
+          .post("/auth/signin")
+          .withBody(dto)
+          .expectStatus(200)
+          // it's gotten from response body ~~~~vv save to in memory pactum store
+          .stores("userAccessToken", "access_token")
+      })
+    });
   })
 
   describe('User', () => {
-    describe("Get me", () => {});
+    describe("Get me", () => {
+      it("Should get current user", () => {
+        return pactum
+          .spec()
+          .withHeaders({
+            // pactum special syntaxt v~~~~~~~~~~~~~~v to get stored data
+            Authorization : "Bearer $S{userAccessToken}"
+          })
+          .post("/user/me")
+          .expectStatus(200)
+      })
+    });
 
     describe('Edit User', () => {});
   });
@@ -49,8 +154,8 @@ describe("App e2e", () => {
 
     describe('Get Bookmarks', ()=>{});
     describe('Get Bookmark by id', ()=>{});
-    describe('Edit Bookmark', ()=>{});
-    describe('Delete Bookmark', ()=>{});
+    describe('Edit Bookmark by id', ()=>{});
+    describe('Delete Bookmark by id', ()=>{});
   });
 
 })
